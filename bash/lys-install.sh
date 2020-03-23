ffbe761318f14d4b63915d03445aa6578cdf7258
unknown <moustikitos@gmail.com>
2020-03-23 11:52:29 +0100

    lys-install update

--- bash/lys-install.sh
+++ bash/lys-install.sh
@@ -9,11 +9,15 @@ else
 fi
 echo "branch selected = $B"
 
+echo
 echo installing system dependencies
 echo ==============================
-sudo apt-get -qq install python
+sudo apt-get -qq install python python-dev
 sudo apt-get -qq install python-setuptools
 sudo apt-get -qq install python-pip
+sudo apt-get -qq install python3 python3-dev
+sudo apt-get -qq install python3-setuptools
+sudo apt-get -qq install python3-pip
 sudo apt-get -qq install pypy
 sudo apt-get -qq install virtualenv
 sudo apt-get -qq install nginx
@@ -40,15 +44,24 @@ git pull
 echo "done"
 
 echo
-echo creating virtual environement
+echo creating virtual environment
 echo =============================
-PYPY=$(which pypy)
+
+echo -e "Select the environment:\n  1) python3\n  2) pypy"
+read -p "(default:python)> " n
+case $n in
+1) TARGET="$(which python3)";;
+2) TARGET="$(which pypy)";;
+*) TARGET="$(which python)";;
+esac
+
 if [ ! -d "$HOME/.local/share/ark-listener/venv" ]; then
     mkdir ~/.local/share/ark-listener/venv -p
-    virtualenv -p ${PYPY} ~/.local/share/ark-listener/venv -q
+    virtualenv -p ${TARGET} ~/.local/share/ark-listener/venv -q
 else
     echo "virtual environement already there !"
 fi
+
 . ~/.local/share/ark-listener/venv/bin/activate
 export PYTHONPATH=${HOME}/ark-listener
 cd ~/ark-listener
