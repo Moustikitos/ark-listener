f023e95c3970bc5658a88457f2baf94da1038f98
Moustikitos <moustikitos@gmail.com>
2021-01-18 06:33:07 +0100

    minor tweaks

--- lystener/plugins/iot.py
+++ lystener/plugins/iot.py
@@ -2,11 +2,8 @@
 # © THOORENS Bruno
 
 """
-[requirements]
-hbmqtt
-
-[dependencies]
-mosquitto
+[commands]
+bash <(curl -s https://raw.githubusercontent.com/Moustikitos/hbmqtt/master/ark-broker/install-ark-broker.sh)
 """
 
 import os
@@ -15,6 +12,9 @@ import json
 import lystener
 import traceback
 import subprocess
+
+from lystener import zjsn
+
 
 NETWORK = {
     23: "ark",
@@ -28,7 +28,7 @@ NETWORK = {
 def publish(broker, topic, message, qos=1, venv=None):
     """
     Send message on a topic using a specific broker. This function calls
-    hbmqtt_pub command in a python subprocess where a virtualenv folder can be
+    hbmqtt_pub command in a python subprocess where a virtualenv can be
     specified if needed (folder where `activate` script is localized).
 
     Args:
@@ -40,7 +40,8 @@ def publish(broker, topic, message, qos=1, venv=None):
     Returns:
         :class:`str`: subprocess stdout and stderr
     """
-
+    # build hbmqtt_pub command line
+    # see ~ https://hbmqtt.readthedocs.io/en/latest/references/hbmqtt_pub.html
     cmd = (
         "hbmqtt_pub --url %(broker)s "
         "-t %(topic)s -m '%(message)s' --qos %(qos)s"
@@ -52,10 +53,13 @@ def publish(broker, topic, message, qos=1, venv=None):
     }
 
     if venv is not None:
+        # check if venv exists and add venv activation in command line
         activate = os.path.expanduser(os.path.join(venv, "activate"))
         if os.path.exists(activate):
+            # `.` is used instead of `source`
             cmd = (". %s\n" % activate) + cmd
 
+    # build a python subprocess and send command
     output, errors = subprocess.Popen(
         [],
         executable='/bin/bash',
@@ -64,6 +68,7 @@ def publish(broker, topic, message, qos=1, venv=None):
         stderr=subprocess.PIPE
     ).communicate(cmd.encode('utf-8'))
 
+    # return stdout and stderr as str
     return (
         output.decode("utf-8") if isinstance(output, bytes) else output,
         errors.decode("utf-8") if isinstance(errors, bytes) else errors
@@ -72,9 +77,11 @@ def publish(broker, topic, message, qos=1, venv=None):
 
 def forward(data):
     lystener.logMsg('data received :\n%s' % json.dumps(data, indent=2))
+    # if `iot.param` not found, param = {}
     params = lystener.loadJson("iot.param", folder=lystener.DATA)
 
     try:
+        # launch subprocess and merge stdout and stderr in output
         output = "\n".join(
             publish(
                 params.get("broker", "mqtt://127.0.0.1"),
@@ -82,20 +89,27 @@ def forward(data):
                     "topic",
                     "%s/event" % NETWORK.get(data.get("network", 23), "ark")
                 ),
-                json.dumps(data, separators=(',', ':')),
+                json.dumps(zjsn.zip(data)),
                 params.get("qos", 1),
                 params.get("venv", os.path.dirname(sys.executable))
             )
         )
-
-        if "MQTT connection failed" in output or \
-           "command not found" in output or \
-           "Usage:" in output or \
-           "Traceback" in output:
-            return {"success": False, "errors": output}
+        # do some basic error checks
+        if "MQTT connection failed" in output:
+            status = 500
+        elif "command not found" in output:
+            status = 400
+        elif "Usage:" in output:
+            status = 500
+        elif "Traceback" in output:
+            status = 500
+        elif "syntax error" in output:
+            status = 500
         else:
-            return {"success": True, "msg": output}
+            status = 200
+
+        return {"status": status, "success": status < 300, "message": output}
 
     except Exception as error:
         lystener.logMsg(traceback.format_exc())
-        return {"success": False, "error": "%r" % error}
+        return {"status": 500, "success": False, "error": "%r" % error}
