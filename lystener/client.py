b3a6b7ef111d8595a8d338df072364c9975af234
Moustikitos <moustikitos@gmail.com>
2021-05-08 17:12:32 +0200

    cSecp256k1 compliancy

--- lystener/client.py
+++ lystener/client.py
@@ -2,96 +2,63 @@
 # © THOORENS Bruno
 
 import os
-import hashlib
-import getpass
 import binascii
-import pySecp256k1 as secp256k1
+import cSecp256k1 as secp256k1
 
 from lystener import rest, getPublicIp
-from secp256k1 import ecdsa, schnorr
 
+ECDSA = None
 
-PRIVKEY = None
 PUBLIC_IP = rest.GET.plain(
     peer="https://www.ipecho.net"
 ).get("raw", getPublicIp())
 
 
 def link(secret=None):
-    global PRIVKEY
-    if secret is None:
-        secret = getpass.getpass(prompt="Type or paste your secret> ")
-    PRIVKEY = binascii.hexlify(
-        secp256k1.hash_sha256(
-            secret if isinstance(secret, bytes) else secret.encode("utf-8")
-        )
-    )
+    global ECDSA
+    ECDSA = secp256k1.Ecdsa(secret)
 
 
 def unlink():
-    global PRIVKEY
-    PRIVKEY = None
+    global ECDSA
+    ECDSA = None
 
 
-def schnorr_sign(msg, privateKey):
-    msg = hashlib.sha256(msg.encode("utf-8")).digest()
-    return binascii.hexlify(
-        schnorr.sign(msg, binascii.unhexlify(privateKey))
-    )
-
-
-def ecdsa_sign(msg, privateKey):
-    msg = hashlib.sha256(msg.encode("utf-8")).digest()
-    return binascii.hexlify(
-        ecdsa.sign(msg, binascii.unhexlify(privateKey))
-    )
-
-
-def create_header(privateKey, payload, schnorr=False, peer=None):
+def create_header(payload, schnorr=False, peer=None):
+    global ECDSA
     salt = binascii.hexlify(os.urandom(32))
     salt = salt.decode("utf-8") if isinstance(salt, bytes) else salt
+    msg = PUBLIC_IP + salt + rest.GET.salt(peer=peer).get("result", "?")
 
     return {
         "Salt": salt,
-        "Method": "schnorr" if schnorr else "ecdsa",
-        "Public-Key": binascii.hexlify(
-            secp256k1.encoded_from_point(
-                secp256k1.PublicKey.from_seed(binascii.unhexlify(privateKey))
-            )
-        ),
-        "Signature": (schnorr_sign if schnorr else ecdsa_sign)(
-            PUBLIC_IP + salt + rest.GET.salt(peer=peer).get("salt", "?"),
-            privateKey
-        )
+        "Public-Key": ECDSA.puk().encode().decode("utf-8"),
+        "Signature": ECDSA.sign(msg).der().decode("utf-8")
     }
 
 
 def secp256k1_filter(**kwargs):
-    privateKey = kwargs.pop("privateKey", PRIVKEY)
+    global ECDSA
     headers = kwargs.pop("headers", {"Content-type": "application/json"})
     schnorr = kwargs.pop("schnorr", False)
     to_jsonify = kwargs.pop("jsonify", None)
 
     peer = kwargs.get("peer", None)
-    if privateKey is not None:
+    if ECDSA is not None:
         if to_jsonify is not None:
-            headers.update(
-                **create_header(privateKey, to_jsonify, schnorr, peer)
-            )
+            headers.update(**create_header(to_jsonify, schnorr, peer))
         else:
-            headers.update(
-                **create_header(privateKey, kwargs, schnorr, peer)
-            )
+            headers.update(**create_header(kwargs, schnorr, peer))
         kwargs["headers"] = headers
 
     return kwargs
 
 
-PUT = rest.EndPoint(
+PUT = rest.req.EndPoint(
     method=lambda *a, **kw:
-        rest.EndPoint._call("PUT", *a, **secp256k1_filter(**kw))
+        rest.req.EndPoint._call("PUT", *a, **secp256k1_filter(**kw))
 )
-DELETE = rest.EndPoint(
+DELETE = rest.req.EndPoint(
     method=lambda *a, **kw:
-        rest.EndPoint._call("DELETE", *a, **secp256k1_filter(**kw))
+        rest.req.EndPoint._call("DELETE", *a, **secp256k1_filter(**kw))
 )
