4959d98b98c9dd9a7573128404bc4040e978df70
Moustikitos <moustikitos@gmail.com>
2021-10-25 20:55:06 +0200

    lys command improvement

--- lystener/rest.py
+++ lystener/rest.py
@@ -34,10 +34,10 @@ PUBLIC_IP = GET.plain(
 
 
 # default peer configuration
-LISTENER_PEER = {"scheme": "http", "ip": PUBLIC_IP, "port": 5001}
-WEBHOOK_PEER = {"scheme": "http", "ip": "127.0.0.1", "port": 4004}
+LISTENER_PEER = {"scheme": "http", "ip": PUBLIC_IP, "port": ":5001"}
+WEBHOOK_PEER = {"scheme": "http", "ip": "127.0.0.1", "port": ":4004"}
 
-# generate defaults peers
+# generate defaults peers if needed
 peers = lystener.loadJson("peer.json", folder=lystener.ROOT)
 LISTENER_PEER.update(peers.get("listener", {}))
 WEBHOOK_PEER.update(peers.get("webhook", {}))
@@ -45,8 +45,7 @@ WEBHOOK_PEER.update(peers.get("webhook", {}))
 # dump peer.json on first import
 lystener.dumpJson(
     {"listener": LISTENER_PEER, "webhook": WEBHOOK_PEER},
-    "peer.json",
-    folder=lystener.ROOT
+    "peer.json", folder=lystener.ROOT
 )
 
-req.EndPoint.peer = "%(scheme)s://%(ip)s:%(port)s" % WEBHOOK_PEER
+req.EndPoint.peer = "%(scheme)s://%(ip)s%(port)s" % WEBHOOK_PEER
