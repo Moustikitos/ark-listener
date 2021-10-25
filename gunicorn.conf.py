7008d3e75c2714bab94bc4771d9eb53bc132e403
Moustikitos <moustikitos@gmail.com>
2021-10-25 20:50:55 +0200

    removed gunicorn.conf action

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,8 @@
 # -*- coding:utf-8 -*-
-from lystener import server, task
-
 
 def post_worker_init(worker):
-    server.DAEMONS = [
-        task.TaskChecker(),
-        task.TaskExecutioner(),
-        task.MessageLogger(),
-        task.FunctionCaller()
-    ]
+    pass
 
 
 def on_exit(server):
-    task.killall()
+    pass
